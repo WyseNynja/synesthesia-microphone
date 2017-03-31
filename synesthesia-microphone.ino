163ce8958d7dce12eff98aafd88e3a2a30696883
matthova <matthova87@gmail.com>
2017-03-30 19:39:45 -0700

    solid

--- synesthesia-microphone.ino
+++ synesthesia-microphone.ino
@@ -4,34 +4,32 @@
 #endif
 #define PIN            20
 #define NUMPIXELS      10
+#define NOISE_FLOOR    30 // aka -<x> decibels
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-// GUItool: begin automatically generated code
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
-//#include <SerialFlash.h>
+#include <SerialFlash.h>
 
-AudioInputI2S             i2s1;           //xy=61,319
-AudioFilterStateVariable  filter1;        //xy=176,319
-AudioFilterStateVariable  filter2;        //xy=294,339
-AudioFilterStateVariable  filter3;        //xy=422,358
-AudioFilterStateVariable  filter4;        //xy=545,352
-AudioOutputI2S            i2s2;           //xy=662,295
-AudioAnalyzeNoteFrequency notefreq1;      //xy=670,339
+AudioInputI2S             i2s1;
+AudioFilterStateVariable  filter1;
+AudioFilterStateVariable  filter2;
+AudioFilterStateVariable  filter3;
+AudioFilterStateVariable  filter4;
+AudioOutputI2S            i2s2;
+AudioAnalyzeNoteFrequency notefreq1;
+AudioAnalyzeRMS           rms_L;
+AudioControlSGTL5000      sgtl5000_1;
 AudioConnection           patchCord1(i2s1, 0, filter1, 0);
 AudioConnection           patchCord2(filter1, 2, filter2, 0);
 AudioConnection           patchCord3(filter2, 2, filter3, 0);
 AudioConnection           patchCord4(filter3, 0, filter4, 0);
 AudioConnection           patchCord5(filter4, 0, notefreq1, 0);
-AudioConnection           patchCord6(filter4, 0, i2s2, 0);
-AudioConnection           patchCord7(filter4, 0, i2s2, 1);
-AudioControlSGTL5000      sgtl5000_1;     //xy=671,465
-
-AudioAnalyzeRMS           rms_L;
+AudioConnection           patchCord6(filter4, 0, i2s2, 0); // L headphone jack
+AudioConnection           patchCord7(filter4, 0, i2s2, 1); // R headphone jack
 AudioConnection           patchCord8(filter4, rms_L);
-// GUItool: end automatically generated code
 
 // 24 Bit RGB values for the LEDs and associated musical note
 const unsigned int COLOR_C = 0xff0000;
@@ -43,84 +41,81 @@ const unsigned int COLOR_A = 0xff8000;
 const unsigned int COLOR_B = 0xff00ff;
 
 void setup() {
-    AudioMemory(30);
+  AudioMemory(30);
 
-    #if defined (__AVR_ATtiny85__)
-        if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-    #endif
-    pixels.begin(); // This initializes the NeoPixel library.
-
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  pixels.begin(); // This initializes the NeoPixel library.
     
-    filter1.frequency(200);
-    filter2.frequency(200);
-    filter3.frequency(2000);
-    filter4.frequency(2000);
+  filter1.frequency(200);
+  filter2.frequency(200);
+  filter3.frequency(2000);
+  filter4.frequency(2000);
 
-    filter1.resonance(1.5);
-    filter2.resonance(1.5);
-    filter3.resonance(1.5);
-    filter4.resonance(1.5);
+  filter1.resonance(1.5);
+  filter2.resonance(1.5);
+  filter3.resonance(1.5);
+  filter4.resonance(1.5);
     
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.9);
+  sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
+  sgtl5000_1.micGain(45); // value from 0 to 63
 
-    sgtl5000_1.enable();
-    sgtl5000_1.volume(0.9);
-    sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
-    sgtl5000_1.micGain(45);
-
-    /*
-     *  Initialize the yin algorithm's absolute
-     *  threshold, 0.15 is good number.
-     */
-    notefreq1.begin(0.15);
+  // Initialize the yin algorithm's absolute threshold, 0.15 is good number.
+  notefreq1.begin(0.15);
 }
 
-elapsedMillis fps;
+elapsedMillis fps;  // Keep track of the time
+
+// The RGB values sent to the lights
 uint16_t red_current = 0;
 uint16_t green_current = 0;
 uint16_t blue_current = 0;
 
+// The value as read by the input
+// We want to drive the current value towards the destination
 uint16_t red_destination = 0;
 uint16_t green_destination = 0;
 uint16_t blue_destination = 0;
 
-const uint16_t incrementAmount = 8000;
-
-unsigned int currentColor = 0;
-float level = 0.0;
+// Move the current value towards the destination by this amount
+const uint16_t incrementAmount = 4000;
 
 uint16_t increment(uint16_t current, uint16_t dest) {
-    if (current != dest) {
-        if (current < dest) {
-            if (0xffff - incrementAmount < current) {
-                current = 0xffff;
-            } else {
-                if (current + incrementAmount > dest) {
-                  current = dest;
-                } else {
-                  current += incrementAmount;
-                }
-            }
+  if (current != dest) {
+    if (current < dest) {
+      if (0xffff - incrementAmount < current) {
+        current = 0xffff;
+      } else {
+        if (current + incrementAmount > dest) {
+          current = dest;
         } else {
-            if (current < incrementAmount) {
-                current = dest;
-            } else {
-                if (current - incrementAmount < dest) {
-                  current = dest;
-                } else {
-                  current -= incrementAmount;
-                }
-            }
+          current += incrementAmount;
         }
+      }
+    } else {
+      if (current < incrementAmount) {
+        current = dest;
+      } else {
+        if (current - incrementAmount < dest) {
+          current = dest;
+        } else {
+          current -= incrementAmount;
+        }
+      }
     }
+  }
 
-    // get rid of any noise
-    if (current <= incrementAmount && dest <= incrementAmount) {
-      current = 0;
-    }
-
-    return current;
+  if (current <= 1000 && dest <= 1000) {
+    current = 0;
+  }
+  
+  return current;
 }
 
+unsigned int current_color = 0;
 void loop() {
     if (notefreq1.available()) {
         // Read the fundamental frequency
@@ -134,37 +129,52 @@ void loop() {
 
         // Set the RGB destination value based on the note read
         switch (finalNote) {
-            case 0:   currentColor = COLOR_C; break; // C
-            case 1:   currentColor = COLOR_C; break; // C#
-            case 2:   currentColor = COLOR_D; break; // D
-            case 3:   currentColor = COLOR_D; break; // D#
-            case 4:   currentColor = COLOR_E; break; // E
-            case 5:   currentColor = COLOR_F; break; // F
-            case 6:   currentColor = COLOR_F; break; // F#
-            case 7:   currentColor = COLOR_G; break; // G
-            case 8:   currentColor = COLOR_G; break; // G#
-            case 9:   currentColor = COLOR_A; break; // A
-            case 10:  currentColor = COLOR_B; break; // A#
-            case 11:  currentColor = COLOR_B; break; // B
+            case 0:  current_color = COLOR_C; break; // C
+            case 1:  current_color = COLOR_C; break; // C#
+            case 2:  current_color = COLOR_D; break; // D
+            case 3:  current_color = COLOR_D; break; // D#
+            case 4:  current_color = COLOR_E; break; // E
+            case 5:  current_color = COLOR_F; break; // F
+            case 6:  current_color = COLOR_F; break; // F#
+            case 7:  current_color = COLOR_G; break; // G
+            case 8:  current_color = COLOR_G; break; // G#
+            case 9:  current_color = COLOR_A; break; // A
+            case 10: current_color = COLOR_B; break; // A#
+            case 11: current_color = COLOR_B; break; // B
         }        
     }
 
+    // Will run roughly every 25 - 27 milliseconds
     if (fps > 24) {
         fps = 0;
 
-        level = rms_L.read() * 1.0;
-        red_destination   = uint16_t((((currentColor >> 16) & 0xff) << 8 ) * level);
-        green_destination = uint16_t((((currentColor >> 8)  & 0xff) << 8 ) * level);
-        blue_destination  = uint16_t((((currentColor >> 0)  & 0xff) << 8 ) * level);
+        // Read the level
+        float level = rms_L.read() * 1.0;
 
-        red_current = increment(red_current, red_destination);
+        // Convert 0.0 to 1.0 to a decibel value
+        level = log10(level) * 20 + NOISE_FLOOR;
+
+        // Convert decibel to a float value where 0 is silent 1.0 is max
+        level = level <= 0 ? 0: level / NOISE_FLOOR;
+
+        // Compensate for LED brightness with an S Curve
+        level = 1 / (1 + pow(2.71828, (-10 * (level - 0.5))));
+        
+        red_destination   = uint16_t((((current_color >> 16) & 0xff) << 8 ) * level);
+        green_destination = uint16_t((((current_color >> 8)  & 0xff) << 8 ) * level);
+        blue_destination  = uint16_t((((current_color >> 0)  & 0xff) << 8 ) * level);
+
+        // Increment RGB
+        red_current   = increment(red_current  , red_destination);
         green_current = increment(green_current, green_destination);
-        blue_current = increment(blue_current, blue_destination);
+        blue_current  = increment(blue_current , blue_destination);
 
+        // Set each pixel
         for(int i=0; i<NUMPIXELS; i++){
             pixels.setPixelColor(i, pixels.Color(byte(red_current >> 8), byte(green_current >> 8), byte(blue_current >> 8)));
         }
-//        Serial.printf("%i %i %i\n", red_current, green_current, blue_current);
+
+        // Write the pixels
         pixels.show(); // This sends the updated pixel color to the hardware.
     }
 }
